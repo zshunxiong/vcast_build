eb4fe29a1ddbe01d67f03908be0c7028d0d854a1
zshunxiong <zshunxiong@gmail.com>
2020-01-02 20:38:14 +0800

    init for display only

---
+++ precache-manifest.45fc9880507e0f71c2dc63da66a91bb8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a7445ea27b0186af8545eb240e81986",
+    "url": "./index.html"
+  },
+  {
+    "revision": "349d4cfdfb085ff2546c",
+    "url": "./static/css/main.1646d716.chunk.css"
+  },
+  {
+    "revision": "2974e72cfae4ab63a41d",
+    "url": "./static/js/2.b0d9ccc4.chunk.js"
+  },
+  {
+    "revision": "349d4cfdfb085ff2546c",
+    "url": "./static/js/main.1ba0bdbf.chunk.js"
+  },
+  {
+    "revision": "7c8d3efd71641e21e8eb",
+    "url": "./static/js/runtime-main.34e71d75.js"
+  }
+]);
